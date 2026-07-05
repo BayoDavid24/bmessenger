85633684b1161292425788705189aa9a86c07c0d
David Bayo <bayo.david024@gmail.com>
2026-07-05 03:30:16 +0100

    feat: implement authentication routing in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,20 +2,29 @@ import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
 import { Button } from '@heroui/react';
 import { ThemeProvider } from './context/ThemeContext';
 import { WallpaperProvider } from './context/WallpaperContext';
-import { Routes, Route } from 'react-router';
+import { Routes, Route, Navigate } from 'react-router';
 import ChatPage from './pages/ChatPage';
 import AuthPage from './pages/Auth';
+import { useAuth } from '@clerk/react';
 
 
 
 
 function App() {
+
+  const {isSignedIn, isLoaded} = useAuth();
+
+  //TODO: Make this a better loading component, maybe a spinner or something
+  if (!isLoaded) return <div>Loading...</div>
+
   return (
     <ThemeProvider>
     <WallpaperProvider> 
       <Routes>
-        <Route path="/" element={<ChatPage />} />
-        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/" element={isSignedIn ? <ChatPage /> : <Navigate to ={"/auth"} replace />} />
+        <Route 
+        path="/auth" 
+        element={!isSignedIn ? <AuthPage /> : <Navigate to="/" replace />} />
       </Routes>
     </WallpaperProvider>
     </ThemeProvider>
